3e55c0400c958d0acc44e0280accd610622f5cd9
dpaulos6 <itzframepvp@outlook.com>
2025-01-02 18:03:59 +0000

    feat: project structure + colors

---
+++ packages/website/src/data/example.ts
@@ -0,0 +1,23 @@
+export const example = {
+  papaplatte: {
+    name: 'Papaplatte',
+    steam: {
+      id: '76561198013760707',
+      name: 'lattensepp'
+    },
+    discord: {
+      id: '169926504657059840',
+      username: 'papaplatte420',
+      partner: true
+    },
+    twitch: {
+      id: 'papaplatte',
+      partner: true
+    },
+    youtube: {
+      handle: '@papaplatte',
+      id: 'UCDmbhGe7-wC1a55l5ZYAZJw',
+      partner: true
+    }
+  }
+}
